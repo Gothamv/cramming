817a948a5bdfe338a33ac8816bf8a713b81fddc4
Gothamv <gowthamv@icloud.com>
2024-07-08 16:23:00 +0200

    bug fixes?

--- cramming/backend/torch_default.py
+++ cramming/backend/torch_default.py
@@ -131,8 +131,9 @@ class DistillTorchEngineMinimal(torch.nn.Module):
         context = self.model.no_sync if self.accumulated_samples < self.current_batch_size else nullcontext
 
         with context():
-            compute_distillation = self.cfg_impl.dist_start < self.steps <= self.cfg_impl.dist_end 
+            compute_distillation = self.cfg_impl.dist_start < self.steps <= self.cfg_impl.dist_end
             outputs = self.forward(**batch, compute_distillation=compute_distillation)
+
             teacher_mlm_loss = outputs["teacher_mlm_loss"]
             student_mlm_loss = outputs["student_mlm_loss"]
             distillation_loss = outputs["distillation_loss"]
--- distill-eval.py
+++ distill-eval.py
@@ -49,7 +49,8 @@ def main_downstream_process(cfg, setup):
             for step, batch in enumerate(task["trainloader"]):
                 # Heavy lifting is moved to engines
                 device_batch = model_engine.to_device(batch, keys=["input_ids", "labels", "attention_mask"])
-                _, loss, _, _ = model_engine.step(device_batch)
+                total_loss, teacher_mlm_loss, student_mlm_loss, distillation_loss = model_engine.step(device_batch)
+                loss = teacher_mlm_loss
                 loss_vals.append(loss.detach())
                 if cfg.dryrun:
                     break
